dda75c2c6e7abcd152cce5a008d9af2a80f4f0f7
betainverse <betainverse@gmail.com>
2014-09-15 14:30:29 -0400

    refactored T2barplots

--- orderparameter/BB_T2.py
+++ orderparameter/BB_T2.py
@@ -37,16 +37,16 @@ sample_name = '20140905 Zn AdcR T2'
 # in milliseconds, and converts them to seconds. 
 rhTable = '/Users/edmonds/git/methyls/orderparameter/T2_peaklists/20140905_T2_DCNZnAdcR_heights.txt'
 FileDirectory = '/Users/edmonds/git/methyls/orderparameter/T2_peaklists/'
-testfile = '20140905_T2_DCNZnAdcR_10.list'
+
 # Filenames for peaklists, keyed by delay length (seconds)
-peaklists = {0.010:'20140905_T2_DCNZnAdcR_10.list', 
-             0.030:'20140905_T2_DCNZnAdcR_30.list', 
-             0.050:'20140905_T2_DCNZnAdcR_50.list', 
-             0.090:'20140905_T2_DCNZnAdcR_90.list',
-             0.110:'20140905_T2_DCNZnAdcR_110.list',
-             0.150:'20140905_T2_DCNZnAdcR_150.list',
-             0.190:'20140905_T2_DCNZnAdcR_190.list',
-             0.210:'20140905_T2_DCNZnAdcR_210.list'}
+#peaklists = {0.010:'20140905_T2_DCNZnAdcR_10.list', 
+#             0.030:'20140905_T2_DCNZnAdcR_30.list', 
+#             0.050:'20140905_T2_DCNZnAdcR_50.list', 
+#             0.090:'20140905_T2_DCNZnAdcR_90.list',
+#             0.110:'20140905_T2_DCNZnAdcR_110.list',
+#             0.150:'20140905_T2_DCNZnAdcR_150.list',
+#             0.190:'20140905_T2_DCNZnAdcR_190.list',
+#             0.210:'20140905_T2_DCNZnAdcR_210.list'}
 
 # Given a column header containing the name of a spectrum and the
 # associated delay, as in '20140905_T2_DCNZnAdcR_10/10', return the delay
@@ -75,6 +75,54 @@ def fitFunc(t,R2,I0):
 def interpolate(delays):
     return np.array([float(x)/1000 for x in range(int(max(delays)*1000))])
 
+
+
+def T2error(assignment,rhDF):
+    delays = rhDF.columns.values
+    heights = rhDF.ix[assignment]
+    T2s = []
+    for k in range(50):
+        generatedheights = np.random.normal(heights,noise)
+        fitParams, fitCovariances = curve_fit(fitFunc, delays, generatedheights)
+        T2s.append(1/fitParams[0])
+    mu,std = norm.fit(T2s)
+    return mu,std    
+
+def calcT2errors(rhDF):
+    delays=rhDF.columns.values
+    assignments = rhDF[delays[0]].keys()
+    intassignments = [int(x[1:-3]) for x in rhDF[delays[0]].keys()]
+    T2values = []
+    T2errors = []
+    for ass in assignments:
+        print ass
+        mu,std = T2error(ass,rhDF)
+        T2values.append(mu)
+        T2errors.append(std)
+    return T2values,T2errors
+
+def T2barplot(rhDF):
+    delays=rhDF.columns.values
+    assignments = rhDF[delays[0]].keys()
+    intassignments = [int(x[1:-3]) for x in rhDF[delays[0]].keys()]
+    T2values,T2errors = calcT2errors(rhDF)
+    fix,ax = plt.subplots()
+    h = plt.bar(intassignments,
+                  T2values,
+                  color='r',
+                  yerr=T2errors)
+    ax.set_ylabel('T2 (s)')
+    ax.set_xlabel('Residue')
+    plt.axis(xmax=ceil((max(intassignments)+1)/10.0)*10)
+    ax.xaxis.set_major_locator(mplticker.MultipleLocator(10))
+    for line in ax.get_xticklines():
+        line.set_marker(mpllines.TICKDOWN)
+    plt.gcf().subplots_adjust(bottom=0.15)
+    ax.set_title(sample_name)
+    plt.savefig(sample_name+'_bar.pdf')
+    plt.show()
+    return T2values,T2errors
+
 def plotcurve(assignment,rhDF):
     # Assume delays are already floats expressed in seconds
     delays = rhDF.columns.values
@@ -96,66 +144,6 @@ def plotcurve(assignment,rhDF):
                  horizontalalignment='left',verticalalignment='top')
     plt.show()
     #plt.savefig(assignment+'.pdf', bbox_inches=0, dpi=600)
-
-def T2error(assignment,rhDF):
-    delays = rhDF.columns.values
-    heights = rhDF.ix[assignment]
-    T2s = []
-    for k in range(500):
-        generatedheights = np.random.normal(heights,noise)
-        fitParams, fitCovariances = curve_fit(fitFunc, delays, generatedheights)
-        T2s.append(1/fitParams[0])
-    mu,std = norm.fit(T2s)
-    #x = np.linspace(0,1,100)
-    #pdf_fitted = norm.pdf(x,loc=mu,scale=std)
-    #plt.figure()
-    #plt.hist(S2s,30,normed=True)
-    #plt.plot(x,pdf_fitted, 'k', linewidth=2)
-    #plt.show()
-    print std
-    return std    
-
-def T2barplot(rhDF):
-    delays=rhDF.columns.values
-    assignments = rhDF[delays[0]].keys()
-    intassignments = [int(x[1:-3]) for x in rhDF[delays[0]].keys()]
-    T2values = []
-    T2errors = []
-    for ass in assignments:
-        print ass
-        heights = rhDF.ix[ass]
-        T2s = []
-        for k in range(50):
-            generatedheights = np.random.normal(heights,noise)
-            fitParams, fitCovariances = curve_fit(fitFunc, delays, generatedheights)
-            T2s.append(1/fitParams[0])
-        mu,std = norm.fit(T2s)
-        T2values.append(mu)
-        T2errors.append(std)
-    fix,ax = plt.subplots()
-    h = plt.bar(intassignments,
-                  T2values,
-                  color='r',
-                  label=ass,
-                  yerr=T2errors)
-    #plt.subplots_adjust(bottom=0.3)
-    #xticks_pos = [0.5*patch.get_width() + patch.get_xy()[0] for patch in h]
-    #plt.xticks(xticks_pos, intassignments, ha='right', rotation=45)
-    ax.set_ylabel('T2 (s)')
-    ax.set_xlabel('Residue')
-    plt.axis(xmax=ceil((max(intassignments)+1)/10.0)*10)
-    ax.xaxis.set_major_locator(mplticker.MultipleLocator(10))
-    for line in ax.get_xticklines():
-        line.set_marker(mpllines.TICKDOWN)
-    plt.gcf().subplots_adjust(bottom=0.15)
-
-    #ax.set_xticks(ind+width)
-    #ax.set_xticklabels(assignments)
-    ax.set_title(sample_name)
-    plt.savefig(sample_name+'_bar.pdf')
-    plt.show()
-    return T2values,T2errors
-        
 
 def plot1curve(assignment,rhDF,T2errors,ax):
     delays = rhDF.columns.values
